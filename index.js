f58774c45979d9ec3563abceba8a19e845b13927
Yurii Makhniuk <yuriymakhniuk@gmail.com>
2024-10-21 12:28:34 +0300

    Added funcs to work with CLI args

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require("node:fs");
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-i, --input <PATH>", "Path to the input file")
+  .option("-o, --output [PATH]", "Path to the output file")
+  .option("-d, --display", "Display the result ");
+program.parse(process.argv);
+
+const options = program.opts();
+
+
+if (!options.input) {
+  throw new Error("Please, specify input file");
+} else if (!fs.existsSync(options.input)){
+    throw new Error("Cannot find input file");
+} else {
+    fs.readFileSync(options.input, { encoding: "utf8" });
+}
+
+if (!options.output && !options.display) {
+  process.exit(0);
+} else {
+  if (options.output) {
+    data = options.output;
+    fs.writeFileSync(options.output, data);
+    console.log(`The result is written to: ${options.output}`);
+} if (options.display) {
+    console.log(data);
+  }
+}
+ 
+
+
+/* if (options.output){
+  fs.writeFileSync(options.output, {encoding: "utf8"});
+} else if (){};
+ 
+    if (options.display){
+      console.log(data);
+    }
+    if (options.output && options.display) {
+        fs.writeFileSync(options.output, data)
+        console.log(data);
+      }
+    if (!options.output && !options.display) {
+      process.exit();
+    }
+
+if (options.display && options.output){
+
+};
+ */
