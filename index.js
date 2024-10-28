aedbb6fc0fe2387b93d176beb715194259f54219
Yurii Makhniuk <yuriymakhniuk@gmail.com>
2024-10-28 14:15:15 +0200

    Fixed error handling

--- index.js
+++ index.js
@@ -3,27 +3,38 @@ const { Command } = require("commander");
 const program = new Command();
 
 program
-  .option("-i, --input <PATH>", "Path to the input file")
-  .option("-o, --output [PATH]", "Path to the output file")
-  .option("-d, --display", "Display the result ");
+  .requiredOption("-i, --input <PATH>", "Path to the input file")
+  .requiredOption("-o, --output [PATH]", "Path to the output file")
+  .option("-d, --display", "Display the result");
 program.parse(process.argv);
 
 const options = program.opts();
 
 if (!options.input) {
-    throw new Error("Please, specify input file");
-  } else if (!fs.existsSync(options.input)){
-      throw new Error("Cannot find input file");
-  }
+  console.error("Please. specify input file");
+  process.exit(1);
+} else if (!fs.existsSync(options.input)) {
+  console.error("Cannot find input file");
+  process.exit(1);
+}
 
-const dataJSON = JSON.parse(fs.readFileSync(options.input, {encoding:"utf8"}));
-const dataFiltered = dataJSON
-.filter(elem => elem.parent === "BS3_BanksLiab")
-.map(elem => `${elem.txten}:${elem.value}`)
-.join("\n")
+const dataJSON = JSON.parse(fs.readFileSync(options.input, { encoding: "utf8" }));
+const dataFiltered = filteredData(dataJSON, "BS3_BanksLiab");
+
+function filteredData(data, parent) {
+  let result = "";
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].parent === parent) {
+      if (result.length > 0) {
+        result += "\n";
+      }
+      result += `${data[i].txten}:${data[i].value}`;
+    }
+  }return result;
+}
 
 if (dataFiltered.length === 0) {
-console.error("No element found");
+  console.error("No element found");
 }
 
 if (!options.output && !options.display) {
@@ -32,29 +43,8 @@ if (!options.output && !options.display) {
   if (options.output) {
     fs.writeFileSync(options.output, dataFiltered);
     console.log(`The result is written to: ${options.output}`);
-} if (options.display) {
+  }
+  if (options.display) {
     console.log(dataFiltered);
   }
 }
- 
-
-
-/* if (options.output){
-  fs.writeFileSync(options.output, {encoding: "utf8"});
-} else if (){};
- 
-    if (options.display){
-      console.log(data);
-    }
-    if (options.output && options.display) {
-        fs.writeFileSync(options.output, data)
-        console.log(data);
-      }
-    if (!options.output && !options.display) {
-      process.exit();
-    }
-
-if (options.display && options.output){
-
-};
- */
